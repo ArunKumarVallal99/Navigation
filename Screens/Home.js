88e820a3a8d8afca84b9d8bc0fc914b7b3a76c73
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-10 21:15:42 +0530

    Stack

---
+++ Screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component } from 'react';
+import {View,Text} from 'react-native';
+
+class HomePage extends Component{
+    render(){
+        return(
+            <View>
+                <Text>HOME PAGE</Text>
+            </View>
+        );
+
+    } 
+}
+export default HomePage;
