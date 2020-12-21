b6a6cb33a4d354246fe819ac419159f3c6fea3df
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-21 17:42:14 +0530

    Axios with all three methods

--- App.js
+++ App.js
@@ -1,50 +1,149 @@
-import React from 'react';
+
+import React, { Component } from 'react';
+
+import {StyleSheet, View, TouchableOpacity, Text} from 'react-native';
+
 import axios from 'axios';
-import {Text,FlatList,View, StyleSheet,ActivityIndicator} from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler';
 
-export default class Requests extends React.Component {
-    constructor()
-    {
-      super ()
-      this.state = {
-        nameList:[],
-        isLoading:true,      }
+class App extends Component {
+    state={
+        data:'',
+        post:'',
+        multi1:'',
+        isloading:true,
     }
-
-    async componentDidMount(){
-        const  response = await axios.get('https://reactnative.dev/movies.json');
-        const datas = await response.data.movies;
-        this.setState({nameList:datas})
-        console.log(this.state.nameList)
-        console.log(this.state.isLoading)
-        console.log(this.state.nameList.length)
+    //Synch
+   getDataUsingSync = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .then(function (response) {
         
+        alert(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        alert(error.message);
+      })
+      .finally(function () {
+        alert('Finally called');
+      });
+  };
+//Async
+ getDataUsingAsync = async () => {
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/posts/1',
+      );
+      this.setState({data:JSON.stringify(response.data)})
+      alert(JSON.stringify(response.data));
+    } catch (error) {
+      alert(error.message);
     }
+  };
 
-    render() {
-        const { nameList, isLoading } = this.state;
-        return (
-           
-            <View styles={styles.container}>
-            {isLoading?(
-                <FlatList
-                data={nameList}
-                keyExtractor={({ id }) => id}
-                renderItem={({ item }) => (
-                    
-                    <Text>id: {item.id},   Name :  {item.title},    ReleaseYear:  {item.releaseYear}</Text>
-                )}
-                />
-                ) :<Text>NO DATA</Text>
-            }
-            </View>
+ async postDataUsingAsync () {
+    try{
+        const response=axios.post('https://jsonplaceholder.typicode.com/posts', {
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      });
+     const data=JSON.stringify((await response).data);
+        //this.setState({post:JSON.stringify((await response).data)})
      
-        );
-    }
+        alert(JSON.stringify((await response).data));
+        
+        
+    }    
+      catch(error) {
+        // handle error
+        alert(error.message);
+      };
+      //console.log(data);
+      //this.setState({post:data});
+  };
+
+   multipleRequestsInSingleCall = () => {
+       //const data='';
+    axios
+      .all([
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts/1')
+          .then(function (response) {
+              //data=JSON.stringify(response.data);
+            console.log('Post 1 : ' + JSON.stringify(response.data));
+            
+          }),
+        axios
+          .get('https://jsonplaceholder.typicode.com/posts/2')
+          .then(function (response) {
+            alert('Post 2 : ' + JSON.stringify(response.data));
+          }),
+      ])
+      .then(
+        axios.spread(function (acct, perms) {
+          console.warn('Both requests are now complete');
+        }),
+      );
+    //  this.setState({multi1:data});
+  };
+render(){
+  return (
+  <ScrollView>
+    <View style={styles.container}>
+      <Text style={{fontSize: 30, textAlign: 'center'}}>
+        Example of Axios Networking in React Native
+      </Text>
+      {/*Running GET Request*/}
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={this.getDataUsingSync}>
+        <Text>Sync Get Call</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={this.getDataUsingAsync}>
+        <Text>Get Data Using Async Await GET</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={this.postDataUsingAsync}>
+        <Text>Post Data Using Async Await</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonStyle}
+        onPress={this.multipleRequestsInSingleCall}>
+        <Text>Multiple Concurrent Requests In Single Call</Text>
+      </TouchableOpacity>
+
+      <Text style={{textAlign: 'center', marginTop: 18}}>
+            {this.state.data} 
+      </Text>
+      <Text style={{textAlign: 'center', marginTop: 18,margin:30,}}>
+            {this.state.post} 
+      </Text>
+    </View>
+    </ScrollView>
+  );
 }
-const styles=StyleSheet.create({
-    container:{
-        padding:20,
-        flex: 1,
-    },
+};
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1,
+    padding: 16,
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    width: '100%',
+    marginTop: 16,
+  },
 });
+
+export default App;
